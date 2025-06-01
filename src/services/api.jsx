dc4c03e869188cacab11ab7e6667ab0b05d0aec6
mageshkb1212 <mageshkb.aiml@gmail.com>
2025-06-01 20:11:48 +0530

    Fixed import path issue and verified folder structure

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,35 +1,36 @@
 import axios from "axios";
-
-const API_URL="https://cricket-live-line1.p.rapidapi.com";
-const HEADERS={
-    "x-rapidapi-host": "cricket-live-line1.p.rapidapi.com",
-    "x-rapidapi-key": "7f19f12bc1msh78abeeebd4ca182p160ed1jsnb3bde86cae5b"
+const API_URL = "https://cricket-live-line1.p.rapidapi.com";
+const HEADERS = {
+  "x-rapidapi-key": "7f19f12bc1msh78abeeebd4ca182p160ed1jsnb3bde86cae5b",
+  "x-rapidapi-host": "cricket-live-line1.p.rapidapi.com",
 };
 
-const getPointsTable = async () => {
-    const options = {
-        method: "GET",
-        url: `${API_URL}/series/336/pointsTable`,
-        headers: HEADERS,
-    };
-    try {
-        const response = await axios.request(options)
-        console.log("response data ", response.data);
+export const getPointsTable = async () => {
+  const options = {
+    method: "GET",
+    url: `${API_URL}/series/336/pointsTable`,
+    headers: HEADERS,
+  };
 
-        return response.data.data.A.map(team => ({
-            team: team.teams,
-            played: team.P,
-            won: team.W,
-            lost: team.L,
-            points: team.pts,
-            nrr: team.NRR,
-            flag: team.flag
-        }))
+  try {
+    let response = await axios.request(options);
+    console.log("Points table data", response.data);
 
-    } catch (error) {
-        console.log("there was error in fetching the data");
-        throw error;
+    if (!response.data?.status || !response.data?.data?.A) {
+      throw new Error("Invalid points table data format");
     }
-}
 
-export { getPointsTable };
+    return response.data.data.A.map((team) => ({
+      team: team.teams,
+      played: team.P,
+      won: team.W,
+      loss: team.L,
+      points: team.Pts,
+      nrr: team.NRR,
+      flag: team.flag,
+    }));
+  } catch (error) {
+    console.error("Error fetching the data");
+    throw error;
+  }
+};
